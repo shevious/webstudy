f1f016a803e2c08113b0afe0763ed84c94986f52
shevious <shevious@yahoo.com>
2019-09-16 05:59:19 +0000

    changed to components

--- swipe/swipable-routes/src/App.js
+++ swipe/swipable-routes/src/App.js
@@ -2,18 +2,50 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SwipeableRoutes from "react-swipeable-routes";
 
-const RedView = () => (
-  <div style={{ height: 300, backgroundColor: "red" }}>Red</div>
-);
-const BlueView = () => (
-  <div style={{ height: 300, backgroundColor: "blue" }}>Blue</div>
-);
-const GreenView = () => (
-  <div style={{ height: 300, backgroundColor: "green" }}>Green</div>
-);
-const YellowView = () => (
-  <div style={{ height: 300, backgroundColor: "yellow" }}>Yellow</div>
-);
+class RedView extends Component {
+  componentDidMount(){
+    console.log("RedView componentDidMount");
+  }
+  render() {
+    return (
+      <div style={{ height: 300, backgroundColor: "red" }}>Red</div>
+    )
+  }
+};
+
+class BlueView extends Component {
+  componentDidMount(){
+    console.log("BlueView componentDidMount");
+  }
+  render() {
+    return (
+      <div style={{ height: 300, backgroundColor: "blue" }}>Red</div>
+    )
+  }
+};
+
+class GreenView extends Component {
+  componentDidMount(){
+    console.log("GreenView componentDidMount");
+  }
+  render() {
+    return (
+      <div style={{ height: 300, backgroundColor: "green" }}>Green</div>
+    )
+  }
+};
+
+class YellowView extends Component {
+  componentDidMount(){
+    console.log("YellowView componentDidMount");
+  }
+  render() {
+    return (
+      <div style={{ height: 300, backgroundColor: "yellow" }}>Yellow</div>
+    )
+  }
+};
+
 const OtherColorView = ({ match }) => (
   <div style={{ height: 300, backgroundColor: match.params.color }}>
     {match.params.color}
@@ -34,7 +66,7 @@ class App extends Component {
             <Link to="/other/saddlebrown">Saddle Brown</Link>
           </div>
 
-          <SwipeableRoutes replace>
+          <SwipeableRoutes>
             <Route path="/red" component={RedView} />
             <Route path="/blue" component={BlueView} />
             <Route path="/green" component={GreenView} />
