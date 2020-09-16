e54f85af359e6de445c3c7f10f073c7b119bdb46
shevious <shevious@yahoo.com>
2020-09-16 15:34:04 +0900

    06

---
+++ deep/basics/06-01-adam.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-4, 4, 21)
+y = np.linspace(-3, 3, 21)
+z = np.array([np.log(0.02*i**2+j**2+0.2) for j in y for i in x])
+
+X, Y = np.meshgrid(x, y)
+Z = z.reshape(21, 21)
+
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+ax.contourf(X, Y, Z)
+plt.show()
---
+++ deep/basics/06-02-opt1.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+from tensorflow.keras.layers import Input, Flatten, Dense, Conv2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+
+'''
+# relu plotting
+w = np.array([0.8,0.7])
+b = 0
+x1 = np.linspace(-1, 1, num=50)
+x2 = np.linspace(-1, 1, num=50)
+
+x1, x2 = np.meshgrid(x1, x2)
+x = np.vstack((x1.flatten(), x2.flatten())).T
+#print(x)
+
+y = np.tensordot(w, x, axes=(0, 1))
+y[y<0] = 0
+y = y.reshape(len(x1), len(x2))
+#print(y)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# Plot the surface.
+surf = ax.plot_surface(x1, x2, y, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+plt.show()
+'''
+w = np.array([1.2,1.0])
+b = 0
+x1 = np.linspace(-5, 5, num=50)
+x2 = np.linspace(-5, 5, num=50)
+
+x1, x2 = np.meshgrid(x1, x2)
+x = np.vstack((x1.flatten(), x2.flatten())).T
+#print(x)
+
+#y = ((x1-3)**2)*((x1+3)**2) -20*x1+ ((x2-3)**2)*(((x2+3))**2)
+y = ((x1)**2)*((x1)**2) + ((x2)**2)*(((x2))**2)
+#y[y<0] = 0
+#y = 1/(1+np.exp(-y))
+y = y.reshape(len(x1), len(x2))
+#print(y)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# Plot the surface.
+surf = ax.plot_surface(x1, x2, y, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+plt.show()
+
+#y = w*x + b
+
+'''
+fig, ax = plt.subplots()
+ret = ax.scatter(x, y, c=y, cmap=cm.coolwarm)
+ax.set_aspect('equal')
+ax.grid(True, which='both')
+ax.axhline(y=0, color='k')
+ax.axvline(x=0, color='k')
+#fig.colorbar(ret, ax=ax);
+ax.axis([0,1,0,1])
+plt.show()
+'''
