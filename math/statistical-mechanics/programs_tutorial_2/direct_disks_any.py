23fadb8fb7cf5bb084684b8494b5de1cee93c7cd
shevious <shevious@yahoo.com>
2021-04-01 12:01:19 +0900

    python 3

--- math/statistical-mechanics/programs_tutorial_2/direct_disks_any.py
+++ math/statistical-mechanics/programs_tutorial_2/direct_disks_any.py
@@ -11,7 +11,7 @@ N = 16
 n_confs = 10 ** 5
 pairs = [(i, j) for i in range(N - 1) for j in range(i + 1, N)]
 eta_max_list = []
-for conf in xrange(n_confs):
+for conf in range(n_confs):
     L = [(random.random(), random.random()) for k in range(N)]
     sigma_max = min(dist(L[i], L[j]) for i, j in pairs) / 2.0
     eta_max = N * math.pi * sigma_max ** 2
@@ -19,8 +19,10 @@ for conf in xrange(n_confs):
 
 # Begin of graphics output
 pylab.figure()
+#n, bins, patches = pylab.hist(eta_max_list, 100, histtype='step', cumulative=-1, 
+#                   log=True, normed=True, label="numerical evaluation of p_accept")
 n, bins, patches = pylab.hist(eta_max_list, 100, histtype='step', cumulative=-1, 
-                   log=True, normed=True, label="numerical evaluation of p_accept")
+                   log=True, label="numerical evaluation of p_accept")
 explaw = [math.exp( - 2.0 * (N - 1) * eta) for eta in bins]
 pylab.plot(bins, explaw, 'r--', linewidth=1.5, label="1st order virial expansion")
 pylab.xlabel('density eta')
