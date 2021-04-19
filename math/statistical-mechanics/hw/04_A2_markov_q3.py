2c23348b49010f1f8d1d078f4cda9535a6b1bbb3
shevious <shevious@yahoo.com>
2021-04-19 15:09:10 +0900

    hw 4

--- math/statistical-mechanics/hw/04_A2_markov_q3.py
+++ math/statistical-mechanics/hw/04_A2_markov_q3.py
@@ -7,7 +7,6 @@ n_hits = 0
 for i in range(n_trials):
     del_x, del_y = random.uniform(-delta, delta), random.uniform(-delta, delta)
     z = random.uniform(-1, 1)
-    #if abs(x + del_x) < 1.0 and abs(y + del_y) < 1.0:
     if (x + del_x)**2 + (y + del_y)**2 < 1.0:
         x, y = x + del_x, y + del_y
     if x**2 + y**2 + z**2 < 1.0: n_hits += 1
