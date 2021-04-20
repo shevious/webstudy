7e7282c4e4cccb64e635518b8c55cc75b0c6c6e4
shevious <shevious@yahoo.com>
2021-04-20 12:53:38 +0900

    python3 fix

--- math/statistical-mechanics/programs_lecture_5/matrix_square_harmonic_movie.py
+++ math/statistical-mechanics/programs_lecture_5/matrix_square_harmonic_movie.py
@@ -14,7 +14,7 @@ def rho_harmonic_trotter(grid, beta):
 x_max = 5.0
 nx = 100
 dx = 2.0 * x_max / (nx - 1)
-x = [i * dx for i in range(-(nx - 1) / 2, nx / 2 + 1)] 
+x = [i * dx for i in range(-(nx - 1) // 2, nx // 2 + 1)] 
 beta_tmp = 2.0 ** (-8)                   # initial value of beta (power of 2)
 beta     = 2.0 ** 0                      # actual value of beta (power of 2)
 rho = rho_harmonic_trotter(x, beta_tmp)  # density matrix at initial beta
