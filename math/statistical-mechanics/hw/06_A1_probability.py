d12b6e20b154eda32d255b897ddd5d48dd73f0b8
shevious <shevious@yahoo.com>
2021-04-26 17:45:01 +0900

    hw 6

--- math/statistical-mechanics/hw/06_A1_probability.py
+++ math/statistical-mechanics/hw/06_A1_probability.py
@@ -6,27 +6,56 @@ def gauss_cut():
         if abs(x) <= 1.0:
             return x
 
+# mean = 0, sigma = 1
+def gauss_2d(x, y):
+    return math.exp(-0.5 * (x**2 + y**2)) / (2*math.pi)
+
 alpha = 0.5
 nsamples = 1000000
 samples_x = []
 samples_y = []
+
 for sample in range(nsamples):
     while True:
-        #x = random.uniform(-1.0, 1.0)
-        #y = random.uniform(-1.0, 1.0)
+        # gaussian sampling
         x = gauss_cut()
         y = gauss_cut()
-        p = math.exp(-0.5 * (x ** 2 + y ** 2) - alpha * (x ** 4 + y ** 4))
+        #p = math.exp(-0.5 * (x ** 2 + y ** 2) - alpha * (x ** 4 + y ** 4)) / (gauss_2d(x, y) / gauss_2d(0, 0))
+        p = math.exp(-alpha * (x ** 4 + y ** 4))
         if random.uniform(0.0, 1.0) < p:
             break
     samples_x.append(x)
     samples_y.append(y)
 
+pylab.figure(figsize=(6,10))
+pylab.subplot(2, 1, 1)
 pylab.hexbin(samples_x, samples_y, gridsize=50, bins=1000)
 pylab.axis([-1.0, 1.0, -1.0, 1.0])
 cb = pylab.colorbar()
 pylab.xlabel('x')
 pylab.ylabel('y')
-pylab.title('A1_1')
+pylab.title('A1_1 gaussian')
+
+samples_x = []
+samples_y = []
+
+for sample in range(nsamples):
+    while True:
+        # uniform sampling
+        x = random.uniform(-1.0, 1.0)
+        y = random.uniform(-1.0, 1.0)
+        p = math.exp(-0.5 * (x ** 2 + y ** 2) - alpha * (x ** 4 + y ** 4)) 
+        if random.uniform(0.0, 1.0) < p:
+            break
+    samples_x.append(x)
+    samples_y.append(y)
+
+pylab.subplot(2, 1, 2)
+pylab.hexbin(samples_x, samples_y, gridsize=50, bins=1000)
+pylab.axis([-1.0, 1.0, -1.0, 1.0])
+cb = pylab.colorbar()
+pylab.xlabel('x')
+pylab.ylabel('y')
+pylab.title('A1_1 uniform')
 pylab.savefig('plot_A1_1.png')
 pylab.show()
